9883a734166bf9f2129554932d55242310185a78
Anubhav Sharma <work925n@gmail.com>
2025-11-09 01:06:07 +0530

    add some other Tags

--- first.py
+++ first.py
@@ -1,1 +1,12 @@
+# That is the First step of Python
 print("Hello World")
+
+# Using simple calculation
+
+a=5           # Integer
+b=10.22       # Float
+c=a+b         # Variable
+d= "Anubhav"  # String
+print(c)
+
+print(d)
